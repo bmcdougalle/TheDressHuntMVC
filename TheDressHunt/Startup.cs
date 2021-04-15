2d203b16afc3284d3eb501a0974798fdb3b20c04
bmcdougalle <bmcdougalle@gmail.com>
2021-04-15 19:48:00 -0400

    initial commit with user roles mostly built out

---
+++ TheDressHunt/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+using TheDressHunt.Models;
+
+[assembly: OwinStartupAttribute(typeof(TheDressHunt.Startup))]
+namespace TheDressHunt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // I am creating admin Role and creating default Admin User
+            if (!roleManager.RoleExists("Admin"))
+            {
+                //admin role create
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //creating admin super user to maintain site
+                var user = new ApplicationUser();
+                user.UserName = "jack";
+                user.Email = "jFrost@gmail.com";
+
+                string userPWD = "Bm+28654856";
+                var chkUser = UserManager.Create(user, userPWD);
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+
+            }
+            //creating manager role
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Manager";
+                roleManager.Create(role);
+            }
+
+            //create user
+            if (!roleManager.RoleExists("User"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "User";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+}
