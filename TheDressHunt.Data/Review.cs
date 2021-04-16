3c36d11665dcfe501c72fc854809d74dd2acd24d
bmcdougalle <bmcdougalle@gmail.com>
2021-04-15 21:59:45 -0400

    Added Review table to Data layer

---
+++ TheDressHunt.Data/Review.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDressHunt.Data
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        [MinLength(2, ErrorMessage = "Must be at least 2 characters long")]
+        [MaxLength(500, ErrorMessage = "Too many characters in this field")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Review")]
+        [MinLength(2, ErrorMessage = "Must be at least 2 characters long")]
+        [MaxLength(500, ErrorMessage = "Too many characters in this field")]
+        public string Content { get; set; }
+
+        public bool HuntRating { get; set; }
+    }
+}
--- TheDressHunt.Data/Shop.cs
+++ TheDressHunt.Data/Shop.cs
@@ -13,6 +13,9 @@ namespace TheDressHunt.Data
         public int ShopId { get; set; }
 
         [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
         [Display(Name = "Shop Name")]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters")]
         [MaxLength(25, ErrorMessage = "Too many characters in this field")]
