0968a539192d54363dba7182ff85014f4e91cae8
bmcdougalle <bmcdougalle@gmail.com>
2021-04-15 22:56:10 -0400

    Added properties to Hunt class and added properties to Create Review class

--- TheDressHunt.Data/Hunt.cs
+++ TheDressHunt.Data/Hunt.cs
@@ -18,7 +18,25 @@ namespace TheDressHunt.Data
         public int? ShopId { get; set; }
         public virtual Shop Shop { get; set; }
 
-        public string Area { get; set; }
+        [Required]
+        [Display(Name = "What's The Occasion?")]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters")]
+        [MaxLength(25, ErrorMessage = "Too many characters in this field")]
+        public string TypeOfOccasion { get; set; }
+
+        [Required]
+        [Display(Name = "Dress Type")]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters")]
+        [MaxLength(25, ErrorMessage = "Too many characters in this field")]
+        public string DressType { get; set; }
+
+        [Required]
+        [Display(Name = "Color Scheme")]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters")]
+        [MaxLength(40, ErrorMessage = "Too many characters in this field")]
+        public string ColorScheme { get; set; }
+
+        public string City { get; set; }
         public virtual ICollection<Shop> Shops { get; set; } = new List<Shop>();
     }
 }
